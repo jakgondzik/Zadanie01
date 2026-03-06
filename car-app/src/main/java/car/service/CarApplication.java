0e1a0b57c3d2733c16b71bb43de440c26ea5480a
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2026-03-06 11:59:02 +0100

    6.03

---
+++ car-app/src/main/java/car/service/CarApplication.java
@@ -0,0 +1,37 @@
+package car.service;
+
+
+import car.model.Dealership;
+import car.repository.CarDao;
+import car.repository.DealershipDao;
+import car.repository.mem.MemCarDao;
+import car.repository.mem.MemDealershipDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class CarApplication {
+    public static void main(String[] args) {
+
+        System.out.println("Lets find cars");
+        ApplicationContext context = new
+        AnnotationConfigApplicationContext("car");
+        DealershipService service = context.getBean(DealershipService.class);
+        DealershipDao dealershipDao = new MemDealershipDao();
+        CarDao carDao = new MemCarDao();
+
+       // DealershipService service = new DealershipServiceBean(carDao, dealershipDao);
+/**/
+
+        DealershipService service2 = context.getBean(DealershipService.class);
+        List<Dealership> dealerships = service.getAllDealerships();
+
+        System.out.println(dealerships.size() + " dealerships:");
+        dealerships.forEach(System.out::println);
+
+        String foo = context.getBean(String.class);
+        System.out.println("Foo string"+foo);
+
+    }
+}
